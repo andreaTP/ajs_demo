e4236e9db9daed24910a0713d107a802838e5c2e
andrea <andrea.peruffo1982@gmail.com>
2017-09-25 11:24:49 +0100

    minor

--- workerClient/webpack.config.js
+++ workerClient/webpack.config.js
@@ -1,14 +1,14 @@
 const path = require("path");
 module.exports = {
   entry: {
-		main: "./main/main.js",
-		worker: "./worker/worker.js"
-	},
-	output: {
-		path: path.join(__dirname, "js"),
-		filename: "[name].out.js",
-		chunkFilename: "[id].chunk.js"
-	},
+    main: "./main/main.js",
+    worker: "./worker/worker.js"
+  },
+  output: {
+    path: path.join(__dirname, "js"),
+    filename: "[name].out.js",
+    chunkFilename: "[id].chunk.js"
+  },
   module: {
     rules: [
       {
