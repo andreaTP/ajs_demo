b3debaf3830367bc5fec88e15879527a61b6f71e
andrea <andrea.peruffo1982@gmail.com>
2017-09-28 15:31:16 +0100

    fisrt refactoring

--- workerClient/main/main.js
+++ workerClient/main/main.js
@@ -13,47 +13,53 @@ const domHandlers = require('../shared/dom-handlers.js')
 
 const elems = new Map()
 
-worker.onmessage = function(e) {
-  if (e.data.create !== undefined) {
-    // console.log("creating "+e.data.create)
-    const elem = createElement(fromJson(e.data))
-    if (elems.has(e.data.create)) {
-      elems
-        .get(e.data.create)
-        .appendChild(elem)
+const uiManagement = function(orElse) {
+  return function(e) {
+    if (e.data.create !== undefined) {
+      // console.log("creating "+e.data.create)
+      const elem = createElement(fromJson(e.data))
+      if (elems.has(e.data.create)) {
+        elems
+          .get(e.data.create)
+          .appendChild(elem)
+      } else {
+        document
+          .getElementById(e.data.create)
+          .appendChild(elem)
+      }
+      elems.set(e.data.id, elem)
+    } else if (e.data.update !== undefined) {
+      // console.log("updating")
+      applyPatch(elems.get(e.data.id), e.data)
+    } else if (e.data.remove !== undefined) {
+      const node = elems.get(e.data.remove)
+
+      try {
+        node.parentNode.removeChild(node)
+      } catch (e) {}
+      try {
+        node.remove()
+      } catch (e) {}
+
+      elems.delete(e.data.remove)
+    } else if (e.data.register) {
+      // console.log("registering function")
+      const elem = elems.get(e.data.id)
+      const funName = e.data.function
+
+      elem.addEventListener(e.data.register, function(event) {
+        const msg = {}
+        msg.id = e.data.id
+        msg.value = domHandlers[funName](event)
+
+        worker.postMessage(msg)
+      }, false)
     } else {
-      document
-        .getElementById(e.data.create)
-        .appendChild(elem)
+      orElse(e)
     }
-    elems.set(e.data.id, elem)
-  } else if (e.data.update !== undefined) {
-    // console.log("updating")
-    applyPatch(elems.get(e.data.id), e.data)
-  } else if (e.data.remove !== undefined) {
-    const node = elems.get(e.data.remove)
-
-    try {
-      node.parentNode.removeChild(node)
-    } catch (e) {}
-    try {
-      node.remove()
-    } catch (e) {}
-
-    elems.delete(e.data.remove)
-  } else if (e.data.register) {
-    // console.log("registering function")
-    const elem = elems.get(e.data.id)
-    const funName = e.data.function
-
-    elem.addEventListener(e.data.register, function(event) {
-      const msg = {}
-      msg.id = e.data.id
-      msg.value = domHandlers[funName](event)
-
-      worker.postMessage(msg)
-    }, false)
-  } else {
-    console.log("unmatched message %o", e.data)
   }
 }
+
+worker.onmessage = uiManagement(function(e) {
+  console.log("unmatched message %o", e.data)
+})
